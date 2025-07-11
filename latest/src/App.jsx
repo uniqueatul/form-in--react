a672e6d50f63f296694921060f704727ee85c685
unknown <atulkumarsingh958@gmail.com>
2025-07-12 02:02:52 +0530

    removed state file

--- latest/src/App.jsx
+++ latest/src/App.jsx
@@ -1,7 +1,7 @@
 
 import './App.css'
 import Form from './atul';
-import Add from './state';
+
 
 function App() {
   
@@ -9,7 +9,7 @@ function App() {
   return (
     <>
       <Form/>
-      <Add/>
+      
 
     </>
   );
--- latest/src/state.jsx
+++
@@ -1,7 +0,0 @@
-
-
-function Add() {
-    return (<h1>hello</h1>  );
-}
-
-export default Add;
